f1a1ffdc2ff10c3d4298d4fb7ecbf32b2a720357
Aayushi <aayushi1806sharma@gmail.com>
2026-02-14 18:39:12 +0530

    encapsulation, getter setter, and static method covered

--- oops_proj.py
+++ oops_proj.py
@@ -1,10 +1,23 @@
 class chatbook:
+
+    __user_id = 1
     def __init__(self):
+        self.id = chatbook.__user_id
+        chatbook.__user_id += 1
         self.__name = "Default"
         self.username = ""
         self.password = ""
         self.loggedin = False
         # self.menu()
+
+    @staticmethod
+    def get_id():
+        return chatbook.__user_id
+    
+    @staticmethod
+    def set_id(value):
+        chatbook.__user_id = value
+
 
     def get_name(self):
         return self.__name
